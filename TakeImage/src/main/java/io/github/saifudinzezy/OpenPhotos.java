fcc0c7c261bd04f59b6e6d51695e6678e20ff271
saifudinzezy <saifudin.zezy7@gmail.com>
2020-08-15 15:30:27 +0700

    git commit -m "Take image from gallery and photo"

---
+++ TakeImage/src/main/java/io/github/saifudinzezy/OpenPhotos.java
@@ -0,0 +1,8 @@
+package io.github.saifudinzezy;
+
+import android.app.Activity;
+
+public interface OpenPhotos {
+    public void choosePhotoFromGallary(Activity activity);
+    public void takePhotoFromCamera(Activity activity);
+}
